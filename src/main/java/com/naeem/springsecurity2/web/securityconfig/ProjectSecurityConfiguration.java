d1548c0d94c5ca365d56de8e4b28d8a70b3c2363
naeemshaikh <naeem5@yahoo.com>
2021-06-15 09:08:44 -0400

    Configured Users with InMemory Authentication.Closes #2

--- src/main/java/com/naeem/springsecurity2/web/securityconfig/ProjectSecurityConfiguration.java
+++ src/main/java/com/naeem/springsecurity2/web/securityconfig/ProjectSecurityConfiguration.java
@@ -4,7 +4,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class ProjectSecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -24,13 +27,25 @@ public class ProjectSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .httpBasic();
     }
 
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication()
+//                .withUser("admin").password("12345").authorities("admin")
+//                .and()
+//                .withUser("user").password("12345").authorities("read")
+//                .and()
+//                .passwordEncoder(NoOpPasswordEncoder.getInstance());
+//    }
+
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("admin").password("12345").authorities("admin")
-                .and()
-                .withUser("user").password("12345").authorities("read")
-                .and()
-                .passwordEncoder(NoOpPasswordEncoder.getInstance());
+        InMemoryUserDetailsManager userDetailsService = new InMemoryUserDetailsManager();
+        UserDetails user = User.withUsername("admin").password("12345").authorities("admin").build();
+        UserDetails user1 = User.withUsername("user").password("12345").authorities("read").build();
+        userDetailsService.createUser(user);
+        userDetailsService.createUser(user1);
+        auth.userDetailsService(userDetailsService);
+
     }
 }
