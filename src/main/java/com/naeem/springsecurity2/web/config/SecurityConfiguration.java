642dbbef2a37423e02b1969620255cf4ba2cfcf0
naeemshaikh <naeem5@yahoo.com>
2021-06-16 01:09:31 -0400

    Bcrypt password encoder. Resolved #5

--- src/main/java/com/naeem/springsecurity2/web/config/SecurityConfiguration.java
+++ src/main/java/com/naeem/springsecurity2/web/config/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
@@ -57,6 +58,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 //    }
     @Bean
     public PasswordEncoder passwordEncoder(){
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 }
